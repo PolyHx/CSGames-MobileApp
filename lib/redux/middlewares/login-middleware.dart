be8d04b96ffca75e0cd89ff48494b9a17d2ea638
Brandon Roberge <brandonroberge@hotmail.com>
2018-12-28 15:04:22 -0500

    Icons + loading overlay in login

--- lib/redux/middlewares/login-middleware.dart
+++ lib/redux/middlewares/login-middleware.dart
@@ -11,50 +11,51 @@ import 'package:redux_epics/redux_epics.dart';
 import 'package:rxdart/rxdart.dart';
 
 class LoginMiddleware implements EpicClass<AppState> {
-  final AuthService _authService;
-  final FirebaseMessaging _firebaseMessaging;
-  final AttendeesService _attendeesService;
+    final AuthService _authService;
+    final FirebaseMessaging _firebaseMessaging;
+    final AttendeesService _attendeesService;
 
-  LoginMiddleware(this._authService, this._firebaseMessaging, this._attendeesService);
+    LoginMiddleware(this._authService, this._firebaseMessaging, this._attendeesService);
 
-  @override
-  Stream call(Stream actions, EpicStore<AppState> store) {
-    return Observable.merge([
-      Observable(actions)
-        .ofType(TypeToken<LoginAction>())
-        .switchMap((action) => _login(action)),
-      Observable(actions)
-        .ofType(TypeToken<LogOut>())
-        .switchMap((action) => _logout(action.context))
-    ]);
-  }
-
-  Stream<dynamic> _login(LoginAction action) async* {
-    try {
-      if (action.email == '' || action.password == '') {
-        yield LoginFail('Empty email or password');
-        return;
-      }
-      await this._authService.login(action.email, action.password);
-      Navigator.pushReplacementNamed(action.context, Routes.HOME);
-    } catch (err) {
-      print('An error occured while trying to login: $err');
-      if (err == 'Unauthenticated') {
-        yield LoginFail('Invalid email or password');
-        return;
-      }
-      else yield LoginFail('Login Failed');
+    @override
+    Stream call(Stream actions, EpicStore<AppState> store) {
+        return Observable.merge([
+            Observable(actions)
+                .ofType(TypeToken<LoginAction>())
+                .switchMap((action) => _login(action)),
+            Observable(actions)
+                .ofType(TypeToken<LogOut>())
+                .switchMap((action) => _logout(action.context))
+        ]);
     }
-  }
 
-  Stream<dynamic> _logout(BuildContext context) async* {
-    try {
-      String token = await _firebaseMessaging.getToken();
-      await _attendeesService.removeFcmToken(token);
-    } catch(err) {
-      print('An error occured while removing the fcm token $err');
+    Stream<dynamic> _login(LoginAction action) async* {
+        try {
+            if (action.email == '' || action.password == '') {
+                yield LoginFail('Empty email or password');
+                return;
+            }
+            await this._authService.login(action.email, action.password);
+            Navigator.pushReplacementNamed(action.context, Routes.HOME);
+        } catch (err) {
+            print('An error occured while trying to login: $err');
+            if (err == 'Unauthenticated') {
+                yield LoginFail('Invalid email or password');
+                return;
+            }
+            else
+                yield LoginFail('Login Failed');
+        }
     }
-    _authService.logout();
-    Navigator.pushReplacementNamed(context, Routes.LOGIN);
-  }
+
+    Stream<dynamic> _logout(BuildContext context) async* {
+        try {
+            String token = await _firebaseMessaging.getToken();
+            await _attendeesService.removeFcmToken(token);
+        } catch (err) {
+            print('An error occured while removing the fcm token $err');
+        }
+        _authService.logout();
+        Navigator.pushReplacementNamed(context, Routes.LOGIN);
+    }
 }
